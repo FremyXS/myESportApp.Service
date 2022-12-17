e489ff68aed8e856e7312cb508881444f969a300
K_Pav <Bopoh.pavel@gmail.com>
2022-12-17 16:25:37 +0500

    Add socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,27 @@
 import express from "express";
+import * as http from "http";
+
 import bodyParser from "body-parser";
 import {RegisterRoutes} from "./routes";
 
 import * as swaggerJson from "./swagger.json";
 import * as swaggerUI from "swagger-ui-express";
+import {RegisterSocket} from "./socket";
 
 export const app = express();
+export const server: http.Server = http.createServer(app);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 RegisterRoutes(app);
+RegisterSocket(server);
 
 app.use(["/openapi", "/docs", "/swagger"], swaggerUI.serve, swaggerUI.setup(swaggerJson));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
+server.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
 ).on('error', (err: any) => {
     if (err.code === 'EADDRINUSE') {
